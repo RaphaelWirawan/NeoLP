f1939d6f8f9fca4ce5572b2b7b4e6e62be7270b8
Raphael Wirawan <alessandrowirawan@gmail.com>
2022-12-10 10:46:51 +0700

    readme+

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*"],
+  theme: {
+    extend: {
+      colors: {
+        verydarkcolor: "#475C7A",
+        yellow: "#FCBB6D",
+        orange: "#D8737F",
+        darkerorange: "#AB6C82",
+        brown: "#685D79",
+        offwhite: "#EFEEEE",
+      },
+    },
+  },
+  plugins: [],
+};
